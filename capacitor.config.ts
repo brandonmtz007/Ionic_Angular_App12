f106dad81110b162b3697bcc5f83bf62d32d2351
brandonmtz007 <brandonmtz179@gmail.com>
2023-12-14 20:00:49 -0600

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'llamadahttpreto',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
